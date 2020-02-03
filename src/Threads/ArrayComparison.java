b1ce9bd7b5ffa5e128a15525a21eace954a11991
Vadim Rybakov <vadimio9217@gmail.com>
2020-02-03 12:22:19 +0300

    with threads

--- src/Threads/ArrayComparison.java
+++ src/Threads/ArrayComparison.java
@@ -1,69 +1,60 @@
 package Threads;
 
 public class ArrayComparison {
+    static Object monitor = new Object();
     static final int size = 10000000;
     static final int h = size / 2;
     static float[] arr = new float[size];
+    static float[] a1 = new float[size];
+    static float[] a2 = new float[h];
+
     static {
         for (int i = 0; i < size; i++) {
             arr[i] = 1;
         }
+        System.arraycopy(arr, 0, a1, 0, size);
     }
-
-    static void simpleFilling(float[] array){
-        long a = System.currentTimeMillis();
-        for (int i = 0; i < size; i++) {
-            array[i] = (float)(array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    static void filling(float[] array, int start, int length){
+        synchronized (monitor){
+            for (int i = start; i < length; i++) {
+                array[i] = (float)(array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
         }
+    }
+    static void simpleFilling(){
+        long a = System.currentTimeMillis();
+        filling(a1,0,size);
         System.out.println(System.currentTimeMillis() - a);
     }
-    static void quickFilling(float[] array) throws InterruptedException {
+    static void quickFilling() throws InterruptedException {
         long a = System.currentTimeMillis();
-        float[] a1 = new float[h];
-        System.arraycopy(array, 0, a1, 0, h);
-        float[] a2 = new float[h];
-        System.arraycopy(array, h, a2, 0, h);
-
-        Runnable r1 = new Runnable() {
+        System.arraycopy(arr, 0, a2, 0, h);
+        Runnable r = new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < h; i++) {
-                    a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
+                if(a2[h - 1] == 1) filling(a2,0,h);
+                else filling(arr,h,size);
             }
         };
-        Thread t1 = new Thread(r1);
+        Thread t1 = new Thread(r);
         t1.start();
         if(t1.isAlive()) t1.join();
-        Runnable r2 = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < h; i++) {
-                    a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
-            }
-        };
-        Thread t2 = new Thread(r2);
+        Thread t2 = new Thread(r);
         t2.start();
         if(t2.isAlive()) t2.join();
-        System.arraycopy(a1, 0, array, 0, h);
-        System.arraycopy(a2, 0, array, h, h);
+        System.arraycopy(a2, 0, arr, 0, h);
         System.out.println(System.currentTimeMillis() - a);
     }
 
     public static void main(String[] args){
-        float[] newArr1 = new float[size];
-        float[] newArr2 = new float[size];
-        System.arraycopy(arr, 0, newArr1, 0, size);
-        System.arraycopy(arr, 0, newArr2, 0, size);
-        simpleFilling(newArr1);
-//        System.out.println(newArr1[0] + " " + newArr1[h-1] + " " + newArr1[h] + " " + newArr1[size-1]);
+        simpleFilling();
         try{
-            quickFilling(newArr2);
-//            System.out.println(newArr2[0] + " " + newArr2[h-1] + " " + newArr2[h] + " " + newArr2[size-1]);
+            quickFilling();
         }
         catch(InterruptedException e){
             e.printStackTrace();
         }
+        System.out.println(a1[0] + " " + a1[h-1] + " " + a1[h] + " " + a1[h+1] + " " + a1[size-1]);
+        System.out.println(arr[0] + " " + arr[h-1] + " " + arr[h] + " " + arr[h+1] + " " + arr[size-1]);
     }
 }
